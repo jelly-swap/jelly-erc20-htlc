aa504afd89fe1c0351b59e1e3cb4c53137ad98e5
arthas168 <pavlovskipetko@abv.bg>
2019-12-20 19:21:00 +0200

    added simple contract

--- test/index.js
+++ test/index.js
@@ -1,12 +1,16 @@
 const HashTimeLock = artifacts.require("HashTimeLock");
+const TokenContract = artifacts.require("DappTokenContract");
+const { id, secret, invalidSecret, mockNewContract } = require("./mockData.js");
 
 // Unit tests wrapper
 contract("HashTimeLock", () => {
   let contractInstance;
+  let tokenInstance;
   let txHash;
 
   beforeEach(async () => {
     contractInstance = await HashTimeLock.new();
+    tokenInstance = await TokenContract.new();
   });
 
   // Deploy contract
@@ -15,5 +19,28 @@ contract("HashTimeLock", () => {
       contractInstance.address !== "",
       `Expected empty string for address, got ${contractInstance.address} instead`
     );
+  });
+
+  // Contract exists
+  it("should return error, because contract doesn't exist yet", async () => {
+    const contractExists = await contractInstance.contractExists(id);
+    assert(!contractExists, `Expected false, got ${contractExists} instead`);
+  });
+
+  // New contract
+  it("should create new contract", async () => {
+
+    const {
+      outputAmount,
+      hashLock,
+      receiverAddress,
+      outputNetwork,
+      outputAddress
+    } = mockNewContract;
+    const newContract = await contractInstance.newContract(
+      ...Object.values(mockNewContract)
+    );
+
+    assert(true);
   });
 });
